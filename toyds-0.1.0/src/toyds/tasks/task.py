acd48f871540eec5a1fb6983f35ec0b9eb089cfb
Harry Coultas Blum <5440566+mogwai@users.noreply.github.com>
2024-03-05 18:11:29 +0000

    Removed dist

--- toyds-0.1.0/src/toyds/tasks/task.py
+++
@@ -1,8 +0,0 @@
-class Task:
-    def __init__(self, *args, **kwargs):
-        self.name =self.__class__.__name__.lower()
-        self.args = args
-        self.kwargs = kwargs
-
-    def __call__(self):
-        raise NotImplementedError
